848b133ca2ac5030f5b8e0b4488613f7b6a6e6b9
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2023-10-13 18:11:21 +0000

    reviewwith JS good practices

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ window.onload = () => {
   console.log("Hello!");
 };
 
+// Function to generate a random excuse:
 let generateExcuse = () => {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
@@ -23,19 +24,11 @@ let generateExcuse = () => {
     "during my lunch",
     "while I was praying"
   ];
-
+  //Generate random indices for each array:
   let whoRandom = Math.floor(Math.random() * who.length);
   let actionRandom = Math.floor(Math.random() * action.length);
   let whatRandom = Math.floor(Math.random() * what.length);
   let whenRandom = Math.floor(Math.random() * when.length);
 
-  return (
-    who[whoRandom] +
-    " " +
-    action[actionRandom] +
-    " " +
-    what[whatRandom] +
-    " " +
-    when[whenRandom]
-  );
+  return `${who[whoRandom]} ${action[actionRandom]} ${what[whatRandom]} ${when[whenRandom]}`;
 };
